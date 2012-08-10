1c75596b4c07989c5fa6fb468618a1dbb18fea60
Jesse <janka102@gmail.com>
2012-08-09 18:53:56 -0700

    Added config.yml and its options

--- src/com/github/janka102/bullseye/Bullseye.java
+++ src/com/github/janka102/bullseye/Bullseye.java
@@ -1,8 +1,16 @@
 package com.github.janka102.bullseye;
 
+import java.util.List;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bullseye extends JavaPlugin {
+	FileConfiguration config;
+	Boolean allowDispensers;
+	static Boolean blacklist;
+	static List<String> blocks;
+	
 	public static BullLogger logger = new BullLogger("Bullseye");
 	public static BullLogger getBullLogger() {
 		return logger;
@@ -12,7 +20,20 @@ public class Bullseye extends JavaPlugin {
 	public void onEnable() {
 		new BullseyeListener(this);
 		new BullseyeSignListener(this);		
+		
+		this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+		config = this.getConfig();
+		
+		allowDispensers = this.getConfig().getBoolean("allowDispensers");
+        blacklist = this.getConfig().getBoolean("blockList.blacklist");
+        blocks = this.getConfig().getStringList("blockList.blocks");
+        
 		logger.info("Bullseye enabled!");
+		
+		for(String blocks : Bullseye.blocks){
+        	logger.info(blocks);
+        }
 	}
 	
 	@Override
